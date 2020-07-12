1e64d6866b9efd88abfd7226b4328ceff1b8d37d
Bikash Subedi <biikashsubedi@gmail.com>
2020-07-12 08:04:15 +0545

    Adding Upto 7 Projects

---
+++ 5. convert celsius to fahrenheit/project.py
@@ -0,0 +1,4 @@
+x = int(input('Enter the Value in Celsius and convert into Fahrenheit: '))
+
+x = round(x*(9/5) + 32)
+print(str(x) + ' F')
---
+++ 6. Convert Fahrenheit to Celsius/project.py
@@ -0,0 +1,4 @@
+x = int(input('Enter the Value in Fahrenheit and convert into Celsius : '))
+
+x = round((x-32) * (9/5))
+print(str(x) + ' C')
